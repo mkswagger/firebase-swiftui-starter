f5acbe491deb0694a1fd9fd3722fdde967e5ef3f
mkswagger <mathangykrishna16@gmail.com>
2024-02-01 01:45:51 +0530

    Initial Commit

---
+++ FirebaseTry/FirebaseTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseTryApp.swift
+//  FirebaseTry
+//
+//  Created by mathangy on 01/02/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
